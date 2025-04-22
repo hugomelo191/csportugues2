d5c76908b907701311bd531abc592e9acae69e88
Hugo Melo <hugomelo1297@icloud.com>
2025-04-22 22:23:41 +0200

    Configura client com Vite, React e Tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000 // Aumenta o limite do tamanho dos chunks
+    chunkSizeWarningLimit: 1000
   }
 })
